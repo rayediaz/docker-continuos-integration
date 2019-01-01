08e4779d86c74457b1f1ac4fef79550a898baf30
Raimer <raimerencarnacion@gmail.com>
2018-12-31 21:59:12 -0400

    Add route api to setup

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
   res.send(`Hello from Node ${process.version}`);
 });
 
+app.get("/api/setup", (req, res) => {
+  res.status(200).send({ message: "setup completed!" });
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
